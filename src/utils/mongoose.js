2cfd768f2790f3740bf47d68b8b565da20b8b48b
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 04:20:16 +0500

    mongoose connectivity function -- configurable

---
+++ src/utils/mongoose.js
@@ -0,0 +1,15 @@
+
+const mongoose = require("mongoose");
+const config = require("../commons/config")
+const connectMongoDB = async () => {
+    const mongoDB = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbCluster}/${config.dbName}?retryWrites=true&w=majority`;
+    try{    
+        await mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+        return true;
+    }catch(error){
+        console.log(error);
+        return false;
+    }
+};
+
+module.exports = { connectMongoDB };
