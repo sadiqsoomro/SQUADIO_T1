225d850f50771b499f5ffd2942da09069fd7261c
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 00:46:18 +0500

    Created custom auth middleware -- to be used in APIs

---
+++ src/middlewares/auth.js
@@ -0,0 +1,15 @@
+const { CustomError } = require('../utils');
+const { ERROR_CODES } = require('../constants');
+const { authToken } = require('../commons/config')
+const adminAuthMiddleware = async (req, res, next) => {
+    try {
+        if (!req.headers.authorization) throw new CustomError(ERROR_CODES.AUTH_TOKEN_NOT_FOUND);
+        const tokens = req.headers.authorization.replace(/^\s+|\s+$/g, "").split(' ');
+        if (tokens && tokens.length > 0 && tokens[0].toLowerCase() === 'bearer' && tokens[1] && tokens[1] !== 'null') {
+            if (tokens[0] === authToken) next();
+            else throw new CustomError(ERROR_CODES.UNAUTHORIZED_ACCESS);
+        } else throw new CustomError(ERROR_CODES.UNAUTHORIZED_ACCESS);
+    } catch (error) {
+        next(error);
+    }
+};
