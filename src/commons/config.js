f0fa0c77e959dd4c676926872cc4bf6e3d429069
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 04:10:04 +0500

    Added mongoDB ENV variables

--- src/commons/config.js
+++ src/commons/config.js
@@ -2,5 +2,9 @@ require('dotenv').config();
 module.exports = config = {
     nodeServicePort : process.env.NODE_SERVICE_PORT || 4000,
     authToken : process.env.AUTH_TOKEN || null,
-    apiVersion : process.env.API_VERSION || "v1"
+    apiVersion : process.env.API_VERSION || "v1",
+    dbUser : process.env.MONGO_DB_USER,
+    dbPassword : process.env.MONGO_DB_PASS,
+    dbName : process.env.MONGO_DB_DATABASE,
+    dbCluster : process.env.MONGO_DB_CLUSTER
 }
