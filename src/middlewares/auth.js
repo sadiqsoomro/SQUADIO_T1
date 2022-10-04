dab2029169f188467f58b96da491f474616eaa26
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 02:55:31 +0500

    Fixed token index on 1 instead of 0

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = authMiddleware = async (req, res, next) => {
         if (!req.headers.authorization) throw new CustomError(ERROR_CODES.AUTH_TOKEN_NOT_FOUND);
         const tokens = req.headers.authorization.replace(/^\s+|\s+$/g, "").split(' ');
         if (tokens && tokens.length > 0 && tokens[0].toLowerCase() === 'bearer' && tokens[1] && tokens[1] !== 'null') {
-            if (tokens[0] === authToken) next();
+            if (tokens[1] === authToken) next();
             else throw new CustomError(ERROR_CODES.UNAUTHORIZED_ACCESS);
         } else throw new CustomError(ERROR_CODES.UNAUTHORIZED_ACCESS);
     } catch (error) {
