4bd5dc16307923500e99bea26947ea27fc5676a1
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 02:24:01 +0500

    bug on- line: 7 Fix: corrected call for middleware

--- src/routes/cache.js
+++ src/routes/cache.js
@@ -4,5 +4,5 @@ const { CacheController } = require('../controllers');
 const cacheRouter = express.Router();
 const authMiddleware  = require('../middlewares/auth');
 
-cacheRouter.get('/cache/:cacheKey', authMiddleware.authMiddleware, CacheController.getCache);
+cacheRouter.get('/cache/:cacheKey', authMiddleware, CacheController.getCache);
 module.exports = cacheRouter;
