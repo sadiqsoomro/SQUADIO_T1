9b3d76f3d146f8c301cb718dbe565d0e455f920f
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 02:56:56 +0500

    Changed approach from using router to APP.

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const cacheRouter = require('./cache');
-const config = require("../commons/config")
-const router = express.Router();
 
 const setRouter = (app) => {
-  router.use(`/${config.apiVersion}`, cacheRouter);
+  cacheRouter(app);
   //this will be used to ping server through cron job since I am using heroku free dyno
   //and server goes idle after every 30 minutes :D
   app.get('/status', (req, res) => res.send('OK'));
