275f7fa9d9d2f4ce63056de27fb23b96eae4b67c
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-04 21:31:06 +0500

    Added basic express router settings. Hardcoded port Added global rejection and exception handlers

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+process.on('unhandledRejection', (err) => {
+    console.error(`Unhandled Rejection Details::${err}`)
+});
+process.on('uncaughtException', (err) => {
+    console.error(`Uncaught Exception Details::${err}`)
+});
+
+const app = express();
+app.server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.server.listen(4000, () => {
+    console.info(`Started server on => http://localhost:${app.server.address().port}`);
+});
+
+module.exports = app;
