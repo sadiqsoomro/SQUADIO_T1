6e0d5de2db2818f137e50ea26b10eeadfed563b6
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 01:29:17 +0500

    Added API version in ENV and default value

--- src/commons/config.js
+++ src/commons/config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 module.exports = config = {
     nodeServicePort : process.env.NODE_SERVICE_PORT || 4000,
-    authToken : process.env.AUTH_TOKEN || null
+    authToken : process.env.AUTH_TOKEN || null,
+    apiVersion : process.env.API_VERSION || "v1"
 }
