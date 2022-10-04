7b1b05f589a1ad716cd23684fbbf70af84579bd1
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 02:18:22 +0500

    Added global error handler as middleware Added API router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const config = require('./commons/config');
+const { setRouter } = require('./route/api');
+const { globalErrorHandler } = require('./utils/response');
 
 process.on('unhandledRejection', (err) => {
     console.error(`Unhandled Rejection Details::${err}`)
@@ -15,6 +17,11 @@ const app = express();
 app.server = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+setRouter(app);
+
+// Added Global Error handler as middleware
+app.use((err, req, res, next) => globalErrorHandler(err, req, res, next));
+
 app.server.listen(config.nodeServicePort, () => {
     console.info(`Started server on => http://localhost:${app.server.address().port}`);
 });
