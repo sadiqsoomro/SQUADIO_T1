df1483dfd9fc3e5de18360ce54da45d09693821f
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 05:42:01 +0500

    Added function to check if TTL from config is positive integer

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,14 @@ const GenerateRandomString = () => {
     return  (uuid.v4().replace(/-/g, "")).toUpperCase();
 }
 
+const isNumeric = (value) => {
+    const bool = /^\d+$/.test(value);
+    if(bool == true) return parseInt(value)
+    else return false;
+}
+
 module.exports = {
-    GenerateRandomString
+    GenerateRandomString,
+    isNumeric
 }
 
