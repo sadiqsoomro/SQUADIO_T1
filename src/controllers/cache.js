b48e783eb9950e2c326b10b6df448133c85fafc7
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 05:02:40 +0500

    Added service handler in controller

--- src/controllers/cache.js
+++ src/controllers/cache.js
@@ -1,11 +1,12 @@
 const { responseHandler } = require('../utils');
 const { SUCCESS_CODES } = require('../constants');
-
+const CacheService = require('../services/cache');
+ 
 class CacheController { 
   static async getCache(req, res, next) {
     try {
       const { cacheKey } = req.params;
-      const result = null;// Add service here
+      const result = await CacheService.getCache(cacheKey);
       return responseHandler({
         response: res,
         ...SUCCESS_CODES.DEFAULT,
