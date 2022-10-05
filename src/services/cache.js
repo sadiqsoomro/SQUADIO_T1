93e2d2b11214a7b1f9ccde5af9c67edcce0b0c13
Sadiq Soomro <sadiq.soomro29@gmail.com>
2022-10-05 06:58:41 +0500

    Added TTL logic as per requirement

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,22 +1,32 @@
 const { ERROR_CODES } = require('../constants');
-const {CustomError} = require('../utils/error');
+const { CustomError } = require('../utils/error');
 const cacheModel = require("../schemas/cache")
 const { cacheTTL } = require("../commons/config")
-const  Utils  = require("../utils/utils")
+const Utils = require("../utils/utils")
 class CacheService {
   static async getCache(cacheKey) {
-      //check if TTL is valid number
-      const ttl = Utils.isNumeric(cacheTTL)
-      if(ttl == false) throw new CustomError(ERROR_CODES.INVALID_TTL);
-      let response = null;
-      response = await cacheModel.findOne({key:cacheKey});;
-      if(response) console.log("Cache Hit");
-      else {
-        console.log("Cache Miss");
-        const saveCache = new cacheModel({key:cacheKey});
-        response = await saveCache.save();
-      }
+    //check if TTL is valid number
+    const ttl = Utils.isNumeric(cacheTTL)
+    if (ttl == false) throw new CustomError(ERROR_CODES.INVALID_TTL);
+    let response = null;
+    response = await cacheModel.findOne({ key: cacheKey });
+    if (!response || !Utils.IsCacheValid(response.ttl)) {
+      console.log("Cache Miss");
+      const randomString = Utils.GenerateRandomString();
+      response = await cacheModel.findOneAndUpdate(
+        { key: cacheKey },
+        {
+          ttl: Utils.GenerateTTL(),
+          value: randomString
+        },
+        { upsert: true }
+      );
+      return randomString;
+    } else {
+      response = await cacheModel.findOneAndUpdate({ key: cacheKey }, { ttl: Utils.GenerateTTL() });
+      console.log("Cache Hit");
       return response.value;
+    }
   }
 }
 
